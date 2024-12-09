4be7a3c5ee77db1a94b7bd0c6b9454e0bd073068
Eren Can Şahin <eren.sahin10@ogr.sakarya.edu.tr>
2024-12-09 19:03:12 +0300

    git ignore

--- WebBerber/obj/Debug/net8.0/WebBerber.AssemblyInfo.cs
+++ WebBerber/obj/Debug/net8.0/WebBerber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7450f3e3ba2215c38f083ee3304086141ddaa1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0210b3c9e61e01e7ba21baf287b1bab406e3d7f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
