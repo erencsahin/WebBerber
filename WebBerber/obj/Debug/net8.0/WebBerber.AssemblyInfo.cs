18c49bb914de2e2c90f102add8fa1585f84bcd01
Eren Can Şahin <eren.sahin10@ogr.sakarya.edu.tr>
2024-12-09 21:33:15 +0300

    try

--- WebBerber/obj/Debug/net8.0/WebBerber.AssemblyInfo.cs
+++ WebBerber/obj/Debug/net8.0/WebBerber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a305d6f08e73b0e6873059a7c1858aa6e60e03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312584ec5412d4f9759d9e51d1ac002c65637042")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBerber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
