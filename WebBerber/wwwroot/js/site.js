b521f79785a034c24b08434d151675404b1b93b6
kaftaryagmur <kaftaryagmur@gmail.com>
2024-12-17 16:03:16 +0300

    Ana sayfa tasarımı iyileştirildi

--- WebBerber/wwwroot/js/site.js
+++ WebBerber/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.getElementById("mainNav");
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 50) { // Scroll mesafesi
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
-    });
-});
+﻿
