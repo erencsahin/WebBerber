90fe1671bd06c45271b4c6d455de55a069e49b90
kaftaryagmur <kaftaryagmur@gmail.com>
2024-12-18 15:54:14 +0300

    hizmetler partial view oluşturuldu

--- WebBerber/Controllers/HomeController.cs
+++ WebBerber/Controllers/HomeController.cs
@@ -1,28 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using WebBerber.Utils; // AppDbContext 
 using WebBerber.Models;
+using System.Linq;
 
 namespace WebBerber.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context; // DbContext
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppDbContext context)
         {
-            _logger = logger;
+            _context = context; 
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var operations = _context.Operations.ToList(); 
+            return View(operations); 
         }
     }
 }
